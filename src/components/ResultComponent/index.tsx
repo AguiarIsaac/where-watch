923898ba18bd839353e69ed92d2d6cc9c978963b
AguiarIsaac <isaacaguiar_@hotmail.com>
2023-03-20 17:00:50 -0300

    init config results

--- src/components/ResultComponent/index.tsx
+++ src/components/ResultComponent/index.tsx
@@ -1,11 +1,50 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Card } from "./components/Card";
 import { CardList, SectionComponent } from "./styles";
+
+const apiKey = process.env.VITE_TMDB_API_KEY
 
 interface ResultProps {
   listMovies: number[],
   listSeries: number[]
 }
 export function ResultComponent({listMovies, listSeries}: ResultProps) {
+
+  const [ foundMovies, setFoundMovies ] = useState([] as any)
+
+  async function getMoreInfos(listIds: number[]) {
+
+    let moviesFiltereds: any = []
+    
+    for( let i = 0; i < listIds.length; i++ ) {
+      await axios.get(`https://api.themoviedb.org/3/movie/${listIds[i]}?api_key=${apiKey}&language=pt-BR`)
+       .then(response => {
+
+        const movieFormated = {
+          genres: response.data.genres,
+          id: response.data.id,
+          overview: response.data.overview,
+          posterPath: response.data.poster_path,
+          releaseDate: response.data.release_date,
+          title: response.data.title,
+          runtime: response.data.runtime
+        }
+        
+        moviesFiltereds = [...moviesFiltereds, movieFormated]
+      })
+
+      setFoundMovies(moviesFiltereds)
+    }
+  }
+
+  useEffect(() => {
+    if(listMovies.length > 0) {
+      
+      getMoreInfos(listMovies)
+    }
+  },[listMovies, listSeries])
+
 
   return (
     <SectionComponent>
